7324443cae5375b65c17e9d804c776c74d47f714
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-03-17 13:21:03 -0500

    Outputing large files to list.

--- deleting_unneeded_files.py
+++ deleting_unneeded_files.py
@@ -4,17 +4,19 @@ For more information, see enclosed project_details.txt file."""
 
 import os
 import send2trash
-import shutil
+
+large_files = []
 
 
-def find_size(search_dir):
-    for dir_name, subfolders, filenames in os.walk(search_dir):
+def find_size(user_dir):
+    for dir_name, subfolders, filenames in os.walk(user_dir):
         for filename in filenames:
             dir_path = f"{dir_name}/{filename}"
             file_size = os.path.getsize(dir_path)
             if file_size >= 100_000_000:
-                print(f"{dir_path}: {file_size}")
+                large_files.append(f"{dir_path}: {file_size}")
+    print(large_files)
 
 
-search_dir = input("Please type path to directory here: ")
-find_size(search_dir)
+dir_path = input("Please type path to directory here: ")
+find_size(dir_path)
