d17891a3f828d4a207e77d960f1a4cf9aac62456
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-03-18 19:41:29 -0500

    Added directory size calculation and more.

--- deleting_unneeded_files.py
+++ deleting_unneeded_files.py
@@ -6,17 +6,42 @@ import os
 import send2trash
 
 large_files = []
+large_dirs = []
+size_limit = int(input("Please type file size limit in bytes: "))
+dir_path = input("Please type path to directory here: ")
 
 
 def find_size(user_dir):
+    """Finds large file sizes and directories. If the directory exceeds limit, user is prompted
+    to examine its contents. If file size exceeds limit, user is given option to keep or
+    delete file."""
     for dir_name, subfolders, filenames in os.walk(user_dir):
+        dir_size = 0
         for filename in filenames:
-            dir_path = f"{dir_name}/{filename}"
-            file_size = os.path.getsize(dir_path)
-            if file_size >= 100_000_000:
-                large_files.append(f"{dir_path}: {file_size}")
-    print(large_files)
+            file_path = f"{dir_name}/{filename}"
+            file_size = os.path.getsize(file_path)
+            dir_size += file_size
+            if file_size >= size_limit:
+                large_files.append(f"{file_path}")
+        if dir_size >= size_limit:
+            large_dirs.append(dir_name)
+
+    if large_dirs is not None:
+        ld = ", ".join(large_dirs)
+        print(f"The following folders exceed the size limit: {ld}")
+        print("Please examine their contents.")
+
+    if large_files is not None:
+        for lf in large_files:
+            print(f"{lf} exceeds size limit.")
+            del_confirm = input(
+                "Type del to send it to the trash or keep to retain file. "
+            )
+            if del_confirm.lower() == "del":
+                print(f"\nSending {lf} to trash.\n")
+                send2trash.send2trash(lf)
+            else:
+                print("File will remain in directory.")
 
 
-dir_path = input("Please type path to directory here: ")
 find_size(dir_path)
