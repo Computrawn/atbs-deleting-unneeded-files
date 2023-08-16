35427f9d5421e4c5528aeaafbd0dcb151c1d2c13
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-16 14:11:54 -0500

    Prep for refactor.

--- deleting_unneeded_files.py
+++ deleting_unneeded_files.py
@@ -13,49 +13,51 @@ logging.basicConfig(
 )
 logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
-large_files = []
-large_dirs = []
-size_limit = int(input("Please type file size limit in bytes: "))
-dir_path = input("Please type path to directory here: ")
 
-
-def find_size(user_dir):
+def find_size(directory):
     """Finds large file sizes and directories. If the directory exceeds limit, user is prompted
-    to examine its contents. If file size exceeds limit, user is given option to keep or
-    delete file."""
-    for dir_name, subfolders, filenames in os.walk(user_dir):
-        dir_size = 0
+    to examine its contents."""
+    large_files = []
+    large_dirs = []
+    user_size = input("Please type file size limit in megabytes: ")
+    bytes_size = int(user_size) * 1000000
+    for directory, _, filenames in os.walk(directory):
+        directory_size = 0
         for filename in filenames:
-            file_path = f"{dir_name}/{filename}"
+            file_path = f"{directory}/{filename}"
             file_size = os.path.getsize(file_path)
-            dir_size += file_size
-            if file_size >= size_limit:
+            directory_size += file_size
+            if file_size >= bytes_size:
                 large_files.append(f"{file_path}")
-        if dir_size >= size_limit:
-            large_dirs.append(dir_name)
+        if directory_size >= bytes_size:
+            large_dirs.append(directory)
 
     if large_dirs:
         ld = ", ".join(large_dirs)
         print(f"The following folders exceed the size limit: {ld}")
+        # print(f"The following files are larger than {size_limit}: {large_files}")
         print("Please examine their contents.")
     else:
-        print("Nothing above size limit found.")
-
-    if large_files:
-        for lf in large_files:
-            print(f"{lf} exceeds size limit.")
-            del_confirm = input(
-                "Type del to send it to the trash or keep to retain file. "
-            )
-            if del_confirm.lower() == "del":
-                print(f"\nSending {lf} to trash.\n")
-                send2trash.send2trash(lf)
-            else:
-                print("File will remain in directory.")
+        print(f"Nothing above {user_size} megabytes found.")
+    print(f"The following files exceed the limit of {user_size} MB:")
+    for filename in large_files:
+        filename = filename.split("/")
+        print(f"* {filename[-1]}")
+    # if large_files:
+    #     for lf in large_files:
+    #         print(f"{lf} exceeds size limit.")
+    #         del_confirm = input(
+    #             "Type del to send it to the trash or keep to retain file. "
+    #         )
+    #         if del_confirm.lower() == "del":
+    #             print(f"\nSending {lf} to trash.\n")
+    #             send2trash.send2trash(lf)
+    #         else:
+    #             print("File will remain in directory.")
 
 
 def main():
-    find_size(dir_path)
+    find_size(input("Please type path to directory here: "))
 
 
 if __name__ == "__main__":
