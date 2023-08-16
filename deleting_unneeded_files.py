fc34cab11b629eea56fd17f661327b96b96ce39d
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-16 15:39:12 -0500

    Finish refactor.

--- deleting_unneeded_files.py
+++ deleting_unneeded_files.py
@@ -11,40 +11,43 @@ logging.basicConfig(
     filename="logging.txt",
     format="%(asctime)s -  %(levelname)s -  %(message)s",
 )
-# logging.disable(logging.CRITICAL)  # Note out to enable logging.
+logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
 
 def validate_path() -> Path:
-    user_path = Path.home() / input("Please type path to directory here: ")
-    if not user_path.is_dir():
-        print("Directory does not exist.")
-        exit()
-    return user_path
+    while True:
+        user_path = Path.home() / input("Please type path to directory here: ")
+        if not user_path.is_dir():
+            print("Directory does not exist.")
+            continue
+        return user_path
 
 
-def find_large_files(directory):
+def get_max_file_size() -> int:
+    megs_size = input("Please type file size limit in megabytes: ")
+    return int(megs_size) * 1000000
+
+
+def find_large_files(directory: Path, size: int) -> list[Path]:
     """Finds large file sizes in specified directory."""
     large_files = []
-    user_size = input("Please type file size limit in megabytes: ")
-    bytes_size = int(user_size) * 1000000
     for directory, _, filenames in os.walk(directory):
-        directory_size = 0
         for filename in filenames:
             file_path = f"{directory}/{filename}"
             file_size = os.path.getsize(file_path)
-            directory_size += file_size
-            if file_size >= bytes_size:
-                large_files.append(f"{file_path}")
-
-    print(f"The following files exceed the limit of {user_size} MB:")
-    for filename in large_files:
-        filename = filename.split("/")
-        print(f"* {filename[-1]}")
+            if file_size >= size:
+                large_files.append(Path(file_path))
+    sorted_files = sorted(large_files)
+    print("Found the following large files:")
+    for filename in sorted_files:
+        print(f"* {filename.name} in {filename.parent}")
+    return sorted_files
 
 
 def main():
     valid_path = validate_path()
-    find_large_files(valid_path)
+    max_file_size = get_max_file_size()
+    find_large_files(valid_path, max_file_size)
 
 
 if __name__ == "__main__":
